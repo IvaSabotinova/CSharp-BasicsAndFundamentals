09747175b6cf9dd1f967720d25e8a337eae5184a
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 00:49:51 +0200

    Add files via upload

---
+++ Basics/Conditional Statements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace T05Firm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hoursNeeded = int.Parse(Console.ReadLine());
+            int daysAvailable = int.Parse(Console.ReadLine());
+            int numberOfWorkersForAOHWork = int.Parse(Console.ReadLine());
+
+            double hoursAvailableForProjectInOfficeHours = daysAvailable * 8 * 0.9;
+            int hoursAOH = numberOfWorkersForAOHWork * 2 * daysAvailable;
+            double totalHoursAvailableForProject = Math.Floor(hoursAvailableForProjectInOfficeHours + hoursAOH);
+
+            if (hoursNeeded <= totalHoursAvailableForProject)
+            {
+                double hoursLeft = totalHoursAvailableForProject - hoursNeeded;
+
+                Console.WriteLine($"Yes!{hoursLeft} hours left.");
+            }
+            else
+            {
+                double hoursShort = hoursNeeded - totalHoursAvailableForProject;
+                Console.WriteLine($"Not enough time!{hoursShort} hours needed.");
+            }
+        }
+    }
+}
