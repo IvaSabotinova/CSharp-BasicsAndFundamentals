ee323f955d7965b921de4d99e903b7304ee1c08f
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 00:58:03 +0200

    Add files via upload

---
+++ Basics/Conditional Statements/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace T08FuelTank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fuelType = Console.ReadLine();
+            double litresInReservoir = double.Parse(Console.ReadLine());
+
+            if (fuelType == "Diesel" && litresInReservoir >= 25)
+            {      
+
+                Console.WriteLine($"You have enough {fuelType}.");
+            }
+            
+            else if (fuelType == "Gasoline" && litresInReservoir >= 25)
+            {
+                
+                Console.WriteLine($"You have enough {fuelType}.");
+            }
+            else if (fuelType == "Gas" && litresInReservoir >= 25)
+            {
+                
+                Console.WriteLine($"You have enough {fuelType}.");
+            }
+            else if (fuelType == "Diesel" && litresInReservoir < 25)
+            {
+               
+                Console.WriteLine($"Fill your tank with {fuelType}!");
+            }
+            else if (fuelType == "Gasoline" && litresInReservoir < 25)
+            {
+                
+                Console.WriteLine($"Fill your tank with {fuelType}!");
+            }
+            else if (fuelType == "Gas" && litresInReservoir < 25)
+            {
+                
+                Console.WriteLine($"Fill your tank with {fuelType}!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid fuel!");
+            }
+        }
+    }
+}
