04fe392a75f6c4ff40ca6dd73cb11d7c437da909
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 00:54:25 +0200

    Add files via upload

---
+++ Basics/Conditional Statements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace T03Harvest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vineyardAreaX = int.Parse(Console.ReadLine());
+            double grapePerSquareMeterY = double.Parse(Console.ReadLine());
+            int wineNeededForSaleZ = int.Parse(Console.ReadLine());
+            int numberOfWorkers = int.Parse(Console.ReadLine());
+
+            double vineyardAreaForWine = vineyardAreaX * 0.40;
+            double kgGrapeTotal = grapePerSquareMeterY * vineyardAreaForWine;
+            double wineLitresProduced = kgGrapeTotal / 2.5;
+
+            if (wineLitresProduced < wineNeededForSaleZ)
+            {
+                double wineLitresShort = Math.Floor(wineNeededForSaleZ - wineLitresProduced);
+
+                Console.WriteLine($"It will be a tough winter! More {wineLitresShort} liters wine needed.");
+            }
+            else
+            {
+                
+
+                Console.WriteLine($"Good harvest this year! Total wine: {Math.Floor(wineLitresProduced)} liters.");
+
+                double wineLitresMoreEven = Math.Ceiling(wineLitresProduced - wineNeededForSaleZ);
+                double wineLitresPerPerson = Math.Ceiling(wineLitresMoreEven / numberOfWorkers);                
+             
+                Console.WriteLine($"{wineLitresMoreEven} liters left -> {wineLitresPerPerson} liters per person.");
+
+                }
+
+
+               }
+    }
+}
