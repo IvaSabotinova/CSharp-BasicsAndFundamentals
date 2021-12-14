5d2303d348f0a46b2f5f7c754d0fbfa919853ccc
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 00:19:33 +0200

    Add files via upload

---
+++ Basics/FirstStepsInCoding/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace T09YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double areaSqM = double.Parse(Console.ReadLine());
+            const double pricePerSqM = 7.61;
+            double finalPrice = areaSqM * pricePerSqM * 0.82;
+            double discount = areaSqM * pricePerSqM * 0.18;
+            Console.WriteLine($"The final price is: {finalPrice:f2} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
