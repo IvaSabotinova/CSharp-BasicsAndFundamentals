b72c34b0331f7d37bf38599258367a8225bcaf18
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 00:52:43 +0200

    Add files via upload

---
+++ Basics/Conditional Statements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace T02SleepyTomCat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ownerRestDays = int.Parse(Console.ReadLine());
+
+            int ownerWorkDays = 365 - ownerRestDays;
+            int playTotalTimeInMinutes = ownerRestDays * 127 + ownerWorkDays * 63;
+
+            if (playTotalTimeInMinutes > 30000)
+            {
+                int minutesAboveNorm = playTotalTimeInMinutes - 30000;
+                int hoursAboveNorm = minutesAboveNorm / 60;
+                int justMinutesAboveNorm = minutesAboveNorm % 60;
+
+                Console.WriteLine("Tom will run away");
+                Console.WriteLine($"{hoursAboveNorm} hours and {justMinutesAboveNorm} minutes more for play");
+            }
+            else
+            { 
+            int minutesBelowNorm = 30000 - playTotalTimeInMinutes;
+            int hoursBelowNorm = minutesBelowNorm / 60;
+                int justMinutesBelowNorm = minutesBelowNorm % 60;
+                Console.WriteLine("Tom sleeps well");
+                Console.WriteLine($"{hoursBelowNorm} hours and {justMinutesBelowNorm} minutes less for play");
+        }
+        }
+    }
+}
