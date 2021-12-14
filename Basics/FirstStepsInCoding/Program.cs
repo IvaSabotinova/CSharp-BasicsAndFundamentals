41d61077448c2b499457c3063ba60b54fd47f8e3
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 00:22:42 +0200

    Add files via upload

---
+++ Basics/FirstStepsInCoding/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace T02RadiansToDegrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if ("caseSensitive" == "CaseSensitive")
+            {
+                Console.WriteLine("Svetlin");
+            }
+            else
+            {
+                Console.WriteLine("Petar");
+            }
+
+        }
+    }
+}
