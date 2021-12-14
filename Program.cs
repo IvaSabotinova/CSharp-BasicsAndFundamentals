bda48f0d9ad504660cbded5e70ac3d5a9a2fc6da
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-14 20:28:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace T07ProjectsCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int numberOfProjects = int.Parse(Console.ReadLine());
+            const int hoursPerProject = 3;
+            int neededHours = numberOfProjects * hoursPerProject;
+
+            Console.WriteLine($"The architect {name} will need {neededHours} hours to complete {numberOfProjects} project/s.");
+        }
+    }
+}
