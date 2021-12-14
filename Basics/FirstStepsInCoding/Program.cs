f03c787bef7f5b69a20bdbd0b38708d5e9c1f17a
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 00:18:45 +0200

    Add files via upload

---
+++ Basics/FirstStepsInCoding/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace T08PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogsNumber = int.Parse(Console.ReadLine());
+            int otherAnimals = int.Parse(Console.ReadLine());
+
+            const double dogFoodPrice = 2.50;
+            const double otherAnimalsFoodPrice = 4.00;
+            double totalPrice = dogsNumber * dogFoodPrice + otherAnimals * otherAnimalsFoodPrice;
+            Console.WriteLine($"{totalPrice} lv.");
+        }
+    }
+}
