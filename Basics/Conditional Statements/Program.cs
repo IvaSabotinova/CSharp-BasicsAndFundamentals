a54e0f5ba5a57cce12ee29594f94c46decd0e13a
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 00:50:46 +0200

    Add files via upload

---
+++ Basics/Conditional Statements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace T06Pets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysAbsence = int.Parse(Console.ReadLine());
+            int kgFoodLeftbyLandlord = int.Parse(Console.ReadLine());
+            double kgPerDayNeededForDog = double.Parse(Console.ReadLine());
+            double kgPerDayNeededForCat = double.Parse(Console.ReadLine());
+            double gramsPerDayNeedeForTurtle = double.Parse(Console.ReadLine());
+
+            double kgFoodNeededForAll = kgPerDayNeededForDog * daysAbsence + kgPerDayNeededForCat * daysAbsence + gramsPerDayNeedeForTurtle * daysAbsence / 1000;
+
+            if (kgFoodLeftbyLandlord >= kgFoodNeededForAll)
+            {
+                double kgFoodLeft = Math.Floor(kgFoodLeftbyLandlord - kgFoodNeededForAll);
+
+                Console.WriteLine($"{kgFoodLeft} kilos of food left.");
+            }
+            else
+            {
+                double kgFoodShort = Math.Ceiling(kgFoodNeededForAll - kgFoodLeftbyLandlord);
+                Console.WriteLine($"{kgFoodShort} more kilos of food are needed.");
+            }
+        }
+    }
+}
