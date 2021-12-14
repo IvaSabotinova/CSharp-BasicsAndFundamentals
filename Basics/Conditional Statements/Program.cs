2c8b5f33a9e34be6a05baead2c0c71db6863bfe4
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 00:48:37 +0200

    Add files via upload

---
+++ Basics/Conditional Statements/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace T04Transport_Price
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kmTotal = int.Parse(Console.ReadLine());
+            string dayOrNight = Console.ReadLine();
+
+            double priceTaxiDay = 0.70 + 0.79 * kmTotal;
+            double priceTaxiNight = 0.70 + 0.90 * kmTotal;
+
+
+            if (kmTotal < 20 && dayOrNight == "day")
+            {
+                Console.WriteLine($"{priceTaxiDay:f2}");
+            }
+            else if (kmTotal < 20 && dayOrNight == "night")
+            {
+                Console.WriteLine($"{priceTaxiNight:f2}");
+            }
+            else if (kmTotal >= 20 && kmTotal < 100 && dayOrNight == "day")
+            {
+
+                double priceBusDayNight = 0.09 * kmTotal;
+                Console.WriteLine($"{priceBusDayNight:f2}");
+            }
+            else if (kmTotal >= 20 && kmTotal < 100 && dayOrNight == "night")
+            {
+
+                double priceBusDayNight = 0.09 * kmTotal;
+                Console.WriteLine($"{priceBusDayNight:f2}");
+            }
+            else
+            {
+                double priceTrainDayNight = 0.06 * kmTotal;
+                Console.WriteLine($"{priceTrainDayNight:f2}");
+            }
+
+        }
+    }
+}
