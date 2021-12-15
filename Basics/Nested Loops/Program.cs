f9269aa885ae4ed0797405b53505a4d35414d4ca
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 12:22:28 +0200

    Add files via upload

---
+++ Basics/Nested Loops/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace T05ChallengeTheWedding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menNumber = int.Parse(Console.ReadLine());
+            int womenNumber = int.Parse(Console.ReadLine());
+            int tableMaxNumber = int.Parse(Console.ReadLine());
+
+            int countOfTables = 0;
+
+            for (int i = 1; i <= menNumber; i++)
+            {
+                for (int j = 1; j <= womenNumber; j++)
+                {
+                    
+                    
+                    Console.Write($"({i} <-> {j}) ");
+                    countOfTables++;
+                    if (countOfTables == tableMaxNumber)
+                    {
+                        return;
+                    }
+
+                    
+                }
+            }
+            
+        }
+    }
+}
