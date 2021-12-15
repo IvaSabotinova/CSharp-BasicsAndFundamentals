4b4b3f43f5ed591e3561549d0d66a42b8a329ede
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 12:20:49 +0200

    Add files via upload

---
+++ Basics/Nested Loops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace T04CarNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startOfInterval = int.Parse(Console.ReadLine());
+            int endOfInterval = int.Parse(Console.ReadLine());
+
+            for (int i = startOfInterval; i <= endOfInterval; i++)
+            {
+                for (int j = startOfInterval; j <= endOfInterval; j++)
+                {
+                    for (int k = startOfInterval; k <= endOfInterval; k++)
+                    {
+                        for (int l = startOfInterval; l <= endOfInterval; l++)
+                        {
+                            if (((i % 2 == 0 && l % 2 != 0) || (i % 2 != 0 && l % 2 == 0)) && i > l && (j + k) % 2 == 0)
+                            {
+                                Console.Write($"{i}{j}{k}{l} ");
+                            
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
