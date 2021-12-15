57edd88ec1d8e76f8553aea8119cfabe6b0baec0
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 12:29:15 +0200

    Add files via upload

---
+++ Basics/Nested Loops/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace T06NameGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int scores = 0;
+            int maxScores = 0;
+            string winnerPlayer = " ";
+
+
+            while (name != "Stop")
+            {
+                
+                for (int i = 0; i < name.Length; i++)
+                {
+                    int number = int.Parse(Console.ReadLine());
+                    char currentLetter = name[i];
+                    if (currentLetter == number)
+                    {
+                        scores += 10;
+                    }
+                    else
+                    {
+                        scores += 2;
+                    }
+                }
+                if (scores >= maxScores)
+                {
+                    maxScores = scores;
+                    winnerPlayer = name;
+                }
+
+                name = Console.ReadLine();
+                scores = 0;
+                
+
+            }
+            Console.WriteLine($"The winner is {winnerPlayer} with {maxScores} points!");
+        }
+    }
+}
