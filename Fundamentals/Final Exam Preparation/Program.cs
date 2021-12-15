f664cdccddf2cbbd20db6d2fbe1eca75f33d6ba2
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-16 01:32:59 +0200

    Add files via upload

---
+++ Fundamentals/Final Exam Preparation/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace T02FancyBarcodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                string input = Console.ReadLine();
+
+                string pattern = @"@[#]+(?<word>[A-Z][A-Za-z0-9]{4,}[A-Z])@[#]+";
+
+                Regex regex = new Regex(pattern);
+
+                bool isValid = regex.IsMatch(input);
+
+                if (isValid)
+                {
+                    Match match = regex.Match(input);
+                    
+                    string myWord = match.Groups["word"].Value.ToString();
+                    string productGroup = String.Empty;
+                    for (int j = 0; j < myWord.Length; j++)
+                    {
+                        if (char.IsDigit(myWord[j]))
+                        {
+                            productGroup += myWord[j];
+                        }
+
+                    }
+
+                    if (productGroup.Length == 0)
+                    {
+                        Console.WriteLine("Product group: 00");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: {productGroup}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+        }
+    }
+}
