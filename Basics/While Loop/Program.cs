85a9dc2e637d19fa39dfffb1b27bb148cae14843
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 11:58:52 +0200

    Add files via upload

---
+++ Basics/While Loop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace T05AverageNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                int figure = int.Parse(Console.ReadLine());
+
+                sum += figure;
+               
+
+            }
+            Console.WriteLine($"{1.0 * sum / number:f2}");
+        }
+    }
+}
