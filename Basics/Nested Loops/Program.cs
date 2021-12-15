e4ced3eb077696b05dc0646c7579f36c588509a7
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 12:17:42 +0200

    Add files via upload

---
+++ Basics/Nested Loops/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace T02LettersCombinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstLetter = Console.ReadLine();
+            string secondLetter = Console.ReadLine();
+            string thirdLetter = Console.ReadLine();
+
+            int count = 0;
+
+            for (int i = char.Parse(firstLetter); i <= char.Parse(secondLetter); i++)
+            {
+                for (int j = char.Parse(firstLetter); j <= char.Parse(secondLetter); j++)
+                {
+                    for (int k = char.Parse(firstLetter); k <= char.Parse(secondLetter); k++)
+                    {
+
+                        if (i != char.Parse(thirdLetter) && j != char.Parse(thirdLetter) && k != char.Parse(thirdLetter))
+                        {
+
+                            
+                            Console.Write($"{(char)i}{(char)j}{(char)k} ");
+                            count++;
+                            
+                        
+                        }
+                    }
+                }
+
+
+            }
+            Console.Write(count);
+        }
+    }
+}
