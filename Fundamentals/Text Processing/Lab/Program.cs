646e73776b7bb2a72e91ebd0a287b9b596184954
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-16 00:07:26 +0200

    Add files via upload

---
+++ Fundamentals/Text Processing/Lab/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace T01ReverseStringsVer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                IEnumerable<char> reversedInput = input.ToArray().Reverse();
+                
+                Console.WriteLine($"{input} = {string.Join("", reversedInput)}");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
