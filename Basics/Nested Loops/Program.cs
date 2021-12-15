521449bde8e2f85252ffe6be65c0404677f8feb5
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 12:33:43 +0200

    Add files via upload

---
+++ Basics/Nested Loops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace T08SecretDoor_sLock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int F1Ceiling = int.Parse(Console.ReadLine());
+            int F2Ceiling = int.Parse(Console.ReadLine());
+            int F3Ceiling = int.Parse(Console.ReadLine());
+
+            
+
+            for (int i = 1; i <= F1Ceiling; i++)
+            {
+                for (int j = 2; j <= F2Ceiling; j++)
+                {
+                    for (int k = 1; k <= F3Ceiling; k++)
+                    {
+                        if (i % 2 == 0 && k % 2 == 0 && j != 4 && j != 6 && j != 8 && j != 9)
+                        {
+                            Console.WriteLine($"{i} {j} {k}");
+                        
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
