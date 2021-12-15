1893bee2d7a842ecdff3a9996ff34ef4ef5f1fca
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 13:30:52 +0200

    Add files via upload

---
+++ Basics/Exam Preparation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace T04Renovation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wallHeight = int.Parse(Console.ReadLine());
+            int wallWidth = int.Parse(Console.ReadLine());
+            int percentOfAllAreaNotToBePainted = int.Parse(Console.ReadLine());
+
+            double totalAreaForPainting = Math.Ceiling((1.0 * wallHeight * wallWidth * 4) * (1.0 * (100 - percentOfAllAreaNotToBePainted) / 100));
+
+            string command = Console.ReadLine();
+
+            while (command != "Tired!")
+            {
+                int paintLitres = int.Parse(command);               
+                totalAreaForPainting -= paintLitres;
+                if (totalAreaForPainting < 0)
+                {
+
+                    Console.WriteLine($"All walls are painted and you have {Math.Abs(totalAreaForPainting)} l paint left!"); break;
+
+                }
+                else if (totalAreaForPainting == 0)
+                {
+
+                    Console.WriteLine("All walls are painted! Great job, Pesho!"); break;
+                }
+
+
+                command = Console.ReadLine();
+            }
+
+            if (totalAreaForPainting > 0)
+            {
+                Console.WriteLine($"{totalAreaForPainting} quadratic m left.");
+            }
+        }
+    }
+}
