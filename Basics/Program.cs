53e197cd6ed0284c4fcc3fc78d032b905bdff2d2
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 13:25:10 +0200

    Add files via upload

---
+++ Basics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace T01Agency_Profit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string airLine = Console.ReadLine();
+            int ticketNumberAdults = int.Parse(Console.ReadLine());
+            int ticketNumberKids = int.Parse(Console.ReadLine());
+            double netTicketPricePerAdult = double.Parse(Console.ReadLine());
+            double serviceTax = double.Parse(Console.ReadLine());
+
+            double ticketPricePerAdult = netTicketPricePerAdult + serviceTax;
+            double ticketPricePerKid = (netTicketPricePerAdult * 0.30) + serviceTax;
+
+            double airLineNetProfit = ((ticketNumberAdults * ticketPricePerAdult) + (ticketNumberKids * ticketPricePerKid)) * 0.20;
+            
+            Console.WriteLine($"The profit of your agency from {airLine} tickets is {airLineNetProfit:f2} lv.");
+
+        }
+    }
+}
