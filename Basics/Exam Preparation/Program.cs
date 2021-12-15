290f15b75c1f75735d90a33394b3a3e2a69547d0
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 13:31:45 +0200

    Add files via upload

---
+++ Basics/Exam Preparation/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace T05BestPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerName = Console.ReadLine();
+
+            int bestGoalResult = 0;
+            string bestPlayer = "";
+
+            while (playerName != "END")
+            {
+                int playerGoalsNumber = int.Parse(Console.ReadLine());
+
+                if (playerGoalsNumber > bestGoalResult)
+                {
+                    bestGoalResult = playerGoalsNumber;
+                    bestPlayer = playerName;
+
+                    if (bestGoalResult >= 10)
+                    {
+                        Console.WriteLine($"{bestPlayer} is the best player!");
+                        Console.WriteLine($"He has scored {bestGoalResult} goals and made a hat-trick !!!"); return;
+                    }
+
+                }
+
+
+                playerName = Console.ReadLine();
+            }
+
+
+            if (bestGoalResult < 3)
+            {
+                Console.WriteLine($"{bestPlayer} is the best player!");
+                Console.WriteLine($"He has scored {bestGoalResult} goals.");
+            }
+
+            else if (bestGoalResult >= 3 && bestGoalResult < 10)
+            {
+                Console.WriteLine($"{bestPlayer} is the best player!");
+                Console.WriteLine($"He has scored {bestGoalResult} goals and made a hat-trick !!!");
+            }
+
+
+
+
+
+        }
+    }
+}
