9d806e62104646d316fe6d974e853bc988ef2daa
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-16 00:16:53 +0200

    Add files via upload

---
+++ Fundamentals/Text Processing/Exercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace T03ExtractFileVer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int indexOfFileName = input.LastIndexOf(@"\") + 1;
+            int indexOfExtension = input.IndexOf(".") + 1;
+            string fileName = input.Substring(indexOfFileName, indexOfExtension-1 - indexOfFileName);
+            string extension = input.Substring(indexOfExtension);
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
