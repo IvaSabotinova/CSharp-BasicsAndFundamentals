3af9574124dad578bfea2a26bbad989f4b1909e5
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 13:23:29 +0200

    Add files via upload

---
+++ Basics/Conditional Statements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T02Comparison1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            double seaTemp = 24.5;
+            if (seaTemp > 25)
+            {
+                Console.WriteLine("Go to the beach!");
+            }
+            else
+            {
+                Console.WriteLine("Go hiking!");
+            }
+        }
+        
+    }
+
+
+}
