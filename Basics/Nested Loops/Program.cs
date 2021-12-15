e9c0663cbc71e67e3095308970dbc5e5eb48e651
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 12:28:05 +0200

    Add files via upload

---
+++ Basics/Nested Loops/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace T06HighJump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialHeight = int.Parse(Console.ReadLine());
+
+            int tihomirHeight = int.Parse(Console.ReadLine());
+
+            int levelHeight = initialHeight - 30;
+            int countFails = 0;
+            int countAllJumps = 1;
+            
+
+            while (initialHeight >= levelHeight)
+
+            {
+
+                if (levelHeight >= tihomirHeight)
+                {
+                    countFails++;
+                    if (countFails == 3)
+                    {
+                        Console.WriteLine($"Tihomir failed at {levelHeight}cm after {countAllJumps} jumps."); return;
+                    }
+
+                }
+                else
+                {
+                    if (levelHeight == initialHeight)
+                    {
+                        break;
+                    }
+                    
+                    levelHeight += 5;                    
+                    countFails = 0;
+                }
+
+
+                tihomirHeight = int.Parse(Console.ReadLine());
+                countAllJumps++;
+                
+
+
+            }
+              Console.WriteLine($"Tihomir succeeded, he jumped over {initialHeight}cm after {countAllJumps} jumps.");
+            }        
+    }
+}
