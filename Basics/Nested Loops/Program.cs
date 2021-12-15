673cd00326dacfec47eabe0881b63b87967c45d0
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 12:35:12 +0200

    Add files via upload

---
+++ Basics/Nested Loops/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace T09SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            int countCombinations = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = start; j <= end; j++)
+                {
+
+                    countCombinations++;
+
+                    if (i + j == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{countCombinations} ({i} + {j} = {magicNumber})"); return;
+                    }
+                }
+                
+            }
+            Console.WriteLine($"{countCombinations} combinations - neither equals {magicNumber}");
+
+
+
+        }
+    }
+}
