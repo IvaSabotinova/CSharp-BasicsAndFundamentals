4088396cd05bb4ff73b5db5066ee0c0beb65cc75
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 13:26:37 +0200

    Add files via upload

---
+++ Basics/Exam Preparation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace T01OscarsCeremony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rent = int.Parse(Console.ReadLine());
+
+            double statuettes = 1.0 * rent * 0.70;
+            double catering = statuettes * 0.85;
+            double sound = catering * 1 / 2;
+
+            double totalExpenses = rent + statuettes + catering + sound;
+            Console.WriteLine($"{totalExpenses:f2}");
+        }
+    }
+}
