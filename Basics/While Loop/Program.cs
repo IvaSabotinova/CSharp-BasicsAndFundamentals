426ddc3ffe623bcae02f37684adbc399351ac3e2
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 11:57:42 +0200

    Add files via upload

---
+++ Basics/While Loop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace T04NumbersDividedby3WithoutReminder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            for (int i = 1; i <= 100; i++)
+            {
+
+
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            
+        }
+    }
+}
