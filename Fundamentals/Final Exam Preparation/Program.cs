d5721542a29df0144b7a7b67c8ca2ab82da58ec0
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-16 01:33:30 +0200

    Add files via upload

---
+++ Fundamentals/Final Exam Preparation/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace T02MirrorWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = Console.ReadLine();
+
+            string pattern = @"([@|#]+)(?<word1>[A-Za-z]{3,})(\1)(\1)(?<word2>[A-Za-z]{3,})(\1)";
+
+            Regex regex = new Regex(pattern);
+
+
+            MatchCollection matches = regex.Matches(text);
+            List<string> pairs = new List<string>();
+
+            if (matches.Count == 0)
+            {
+                Console.WriteLine("No word pairs found!");
+
+            }
+            else
+            {
+                Console.WriteLine($"{matches.Count} word pairs found!");
+                foreach (Match match in matches)
+                {
+                    string word1 = match.Groups["word1"].Value.ToString();
+                    string word2 = match.Groups["word2"].Value.ToString();
+                    string reversedWord1 = String.Empty;
+                    for (int i = word1.Length - 1; i >= 0; i--)
+                    {
+                        reversedWord1 += word1[i];
+
+                    }
+
+                    if (reversedWord1 == word2)
+                    {
+                        string newString = $"{word1} <=> {word2}";
+                        pairs.Add(newString);
+                    }
+                }
+            }
+
+            if (pairs.Count == 0)
+            {
+                Console.WriteLine("No mirror words!");
+            }
+            else
+            {
+                Console.WriteLine("The mirror words are:");
+                Console.WriteLine(string.Join(", ", pairs));
+            }
+
+
+        }
+    }
+}
