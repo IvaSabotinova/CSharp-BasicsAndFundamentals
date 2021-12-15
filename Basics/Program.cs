b2b2b5118ab2df160ce348d1adbbfd3f3f82bb96
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 13:35:56 +0200

    Add files via upload

---
+++ Basics/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace T01ChristmasPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rollPaperNumber = int.Parse(Console.ReadLine());
+            int rollClothNumber = int.Parse(Console.ReadLine());
+            double litresGlue = double.Parse(Console.ReadLine());
+            int percentDiscount = int.Parse(Console.ReadLine());
+
+            
+
+            double totalExpenses = (rollPaperNumber * 5.80 + rollClothNumber * 7.20 + litresGlue * 1.20) * (1.0 * (100 - percentDiscount) / 100);
+            
+
+            Console.WriteLine($"{totalExpenses:f3}");
+        }
+    }
+}
