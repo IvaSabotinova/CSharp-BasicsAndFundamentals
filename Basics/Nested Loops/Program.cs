77fc2d9b2065a213440e75cdebe7b29f7651bc06
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 12:26:20 +0200

    Add files via upload

---
+++ Basics/Nested Loops/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace T06EasterCompetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCooks = int.Parse(Console.ReadLine());
+            int maxScoresPerCook = 0;
+            string winner = " ";
+
+            for (int i = 1; i <= numberOfCooks; i++)
+            {
+                string cookName = Console.ReadLine();
+                string score = Console.ReadLine();
+
+                int scoresPerCook = 0;
+                      
+                while (score != "Stop")
+                {
+                    int visitorScores = int.Parse(score);
+
+                    scoresPerCook += visitorScores;
+
+                    score = Console.ReadLine();
+                }
+
+                Console.WriteLine($"{cookName} has {scoresPerCook} points.");
+
+                if (maxScoresPerCook < scoresPerCook)
+                {
+                    maxScoresPerCook = scoresPerCook;
+                    winner = cookName;
+                    Console.WriteLine($"{winner} is the new number 1!");
+                }
+
+            }
+
+            Console.WriteLine($"{winner} won competition with {maxScoresPerCook} points!");
+        }
+    }
+}
