54c76d44d8917d62d96a7998cb86b96d0ef79aa1
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 13:28:00 +0200

    Add files via upload

---
+++ Basics/Exam Preparation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace T02MountainRun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double worldRecordSec = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double georgeTimeSecPerMeter = double.Parse(Console.ReadLine());
+
+            double slopeDelaySec = Math.Floor(distance / 50) * 30;
+
+            double georgeTime = distance * georgeTimeSecPerMeter + slopeDelaySec;
+
+            if (georgeTime < worldRecordSec)
+            {
+                Console.WriteLine($"Yes! The new record is {georgeTime:f2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No! He was {(georgeTime - worldRecordSec):f2} seconds slower.");
+            }
+
+        }
+    }
+}
