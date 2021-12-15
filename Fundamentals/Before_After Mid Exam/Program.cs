f9e5eaf3ee3b7fc6ffe9ef67ca3f469f2b4dbe37
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-16 00:55:39 +0200

    Add files via upload

---
+++ Fundamentals/Before_After Mid Exam/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MidExamPrep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfDaysPlunder = int.Parse(Console.ReadLine());
+            int dailyPlunder = int.Parse(Console.ReadLine());
+            double expectedPlunder = double.Parse(Console.ReadLine());
+
+            double totalPlunder = 0;
+
+            for (int i = 1; i <= numberOfDaysPlunder; i++)
+            {
+                totalPlunder += dailyPlunder;
+                if (i % 3 == 0)
+                {
+                    totalPlunder += dailyPlunder * 0.50;
+                }
+
+                if (i % 5 == 0)
+                {
+                    totalPlunder *= 0.70;
+                }
+            }
+
+            if (totalPlunder >= expectedPlunder)
+            {
+                Console.WriteLine($"Ahoy! {totalPlunder:f2} plunder gained.");
+            }
+            else
+            {
+                Console.WriteLine($"Collected only {totalPlunder / expectedPlunder*100:f2}% of the plunder.");
+            }
+        }
+    }
+}
