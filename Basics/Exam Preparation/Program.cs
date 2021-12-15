e4c809c98fb59f5b695200df9d0b8e4e1c7b583d
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 13:27:22 +0200

    Add files via upload

---
+++ Basics/Exam Preparation/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace T02AddBags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double luggageTaxOver20kg = double.Parse(Console.ReadLine());
+            double luggageWeight = double.Parse(Console.ReadLine());
+            int daysTillFlight = int.Parse(Console.ReadLine());
+            int luggageNumber = int.Parse(Console.ReadLine());
+
+            double luggageTaxBelow10kg = luggageTaxOver20kg * 0.20;
+            double luggageTaxBetween10And20kg = luggageTaxOver20kg * 0.50;
+
+            if (daysTillFlight > 30)
+            {
+                if (luggageWeight < 10)
+                {
+                    Console.WriteLine($"The total price of bags is: {((luggageTaxBelow10kg * luggageNumber) * 1.10):f2} lv.");
+                }
+                else if (luggageWeight >= 10 && luggageWeight <= 20)
+                {
+                    Console.WriteLine($"The total price of bags is: {((luggageTaxBetween10And20kg * luggageNumber) * 1.10):f2} lv.");
+                }
+                else
+                {
+                    Console.WriteLine($"The total price of bags is: {((luggageTaxOver20kg * luggageNumber) * 1.10):f2} lv.");
+                }
+            }
+            if (daysTillFlight <= 30 && daysTillFlight >=7)
+            {
+                if (luggageWeight < 10)
+                {
+                    Console.WriteLine($"The total price of bags is: {((luggageTaxBelow10kg * luggageNumber) * 1.15):f2} lv.");
+                }
+                else if (luggageWeight >= 10 && luggageWeight <= 20)
+                {
+                    Console.WriteLine($"The total price of bags is: {((luggageTaxBetween10And20kg * luggageNumber) * 1.15):f2} lv.");
+                }
+                else
+                {
+                    Console.WriteLine($"The total price of bags is: {((luggageTaxOver20kg * luggageNumber) * 1.15):f2} lv.");
+                }
+            }
+            if (daysTillFlight <7)
+            {
+                if (luggageWeight < 10)
+                {
+                    Console.WriteLine($"The total price of bags is: {((luggageTaxBelow10kg * luggageNumber) * 1.40):f2} lv.");
+                }
+                else if (luggageWeight >= 10 && luggageWeight <= 20)
+                {
+                    Console.WriteLine($"The total price of bags is: {((luggageTaxBetween10And20kg * luggageNumber) * 1.40):f2} lv.");
+                }
+                else
+                {
+                    Console.WriteLine($"The total price of bags is: {((luggageTaxOver20kg * luggageNumber) * 1.40):f2} lv.");
+                }
+            }
+
+
+        }
+    }
+}
