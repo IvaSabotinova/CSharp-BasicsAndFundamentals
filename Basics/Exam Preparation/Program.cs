d1c5974805534dc88d35f65e74dffab98fb16a5e
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2021-12-15 13:33:19 +0200

    Add files via upload

---
+++ Basics/Exam Preparation/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace T05Oscars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string actorName = Console.ReadLine();
+            double academyScores = double.Parse(Console.ReadLine());
+            int jouryNumber = int.Parse(Console.ReadLine());
+
+            double actorScores = academyScores;
+
+            for (int i = 1; i <= jouryNumber; i++)
+            
+            {
+                string jouryMemberName = Console.ReadLine();
+                double jouryMemberScores = double.Parse(Console.ReadLine());
+
+                int jouryMemberNameLenght = jouryMemberName.Length;
+
+                actorScores += jouryMemberNameLenght * jouryMemberScores / 2;
+                if (actorScores > 1250.5)
+                {
+                    Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {actorScores:f1}!"); break;
+                }
+
+            }
+            
+
+            if (actorScores <= 1250.5)
+            {
+            
+            
+                Console.WriteLine($"Sorry, {actorName} you need {(1250.5 - actorScores):f1} more!");
+            }
+
+        }
+    }
+}
